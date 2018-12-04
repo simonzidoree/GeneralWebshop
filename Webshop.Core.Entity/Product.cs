042f9b34503ce6fde0b46fb159bd47e74817fde5
Thomas <thom170b@easv365.dk>
2018-12-04 04:58:25 +0100

    Added Product-entity with properties

---
+++ Webshop.Core.Entity/Product.cs
@@ -0,0 +1,13 @@
+namespace Webshop.Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public int AmountInStock { get; set; }
+        public bool Featured { get; set; }
+    }
+}
